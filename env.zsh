f4bbe3099d55c33a4b43e422a692a0f26b43e088
0xnofoot <x0xnofoot@163.com>
2024-01-12 13:58:52 +0800

    add env about QT_WAYLAND

--- env.zsh
+++ env.zsh
@@ -6,3 +6,8 @@ if [ ! -d "$TMP_DIR" ]; then
     mkdir "$TMP_DIR"
     echo "make ~/.tmp"
 fi
+
+export GDK_SCALE=1.5
+export QT_QPA_PALTFORM=wayland
+export QT_WAYLAND_FORCE_DPI=144
+export QT_SCALE_FACTOR=1.5
