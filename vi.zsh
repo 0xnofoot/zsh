52b5b23a9ba73ac4b87a450f67e05a9600953ba9
NoFoot <x0xnofoot@163.com>
2024-01-18 14:48:00 +0800

    enhance vi-mode

--- vi.zsh
+++ vi.zsh
@@ -1,11 +1,78 @@
 bindkey -v
 
+bindkey -M vicmd "H" remap_vi_H
+bindkey -M vicmd "L" remap_vi_L
+bindkey -M vicmd "W" remap_vi_W
+bindkey -M vicmd "E" remap_vi_E
+bindkey -M vicmd "B" remap_vi_B
+bindkey -M vicmd "," vi-beginning-of-line
+bindkey -M vicmd "." vi-end-of-line
+bindkey -M vicmd "'" vi-match-bracket
+
+remap_vi_H() {
+    for i in {1..3}
+    do
+        zle vi-backward-char
+    done
+}
+zle -N remap_vi_H
+
+remap_vi_L() {
+    for i in {1..3}
+    do
+        zle vi-forward-char
+    done
+}
+zle -N remap_vi_L
+
+remap_vi_W() {
+    for i in {1..3}
+    do
+        zle vi-forward-blank-word
+    done
+}
+zle -N remap_vi_W
+
+remap_vi_E() {
+    for i in {1..3}
+    do
+        zle vi-forward-blank-word-end
+    done
+}
+zle -N remap_vi_E
+
+remap_vi_B() {
+    for i in {1..3}
+    do
+        zle vi-backward-blank-word
+    done
+}
+zle -N remap_vi_B
+
+# ci", ci', ci`, di", etc
+autoload -U select-quoted
+zle -N select-quoted
+for m in visual viopp; do
+    for c in {a,i}{\',\",\`}; do
+        bindkey -M $m $c select-quoted
+    done
+done
+
+# ci{, ci(, ci<, di{, etc
+autoload -U select-bracketed
+zle -N select-bracketed
+for m in visual viopp; do
+    for c in {a,i}${(s..)^:-'()[]{}<>bB'}; do
+        bindkey -M $m $c select-bracketed
+    done
+done
+
 function zle-keymap-select {
     if [[ ${KEYMAP} == vicmd ]] || [[ $1 = 'block' ]]; then
         echo -ne '\e[1 q'
     elif [[ ${KEYMAP} == main ]] || [[ ${KEYMAP} == viins ]] || [[ ${KEYMAP} = '' ]] || [[ $1 = 'beam' ]]; then
         echo -ne '\e[5 q'
-  fi
+    fi
 }
 zle -N zle-keymap-select
 
