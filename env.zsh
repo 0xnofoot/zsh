c6f2e276d504563a5279ca6c26217cbe0db1e8ef
NoFoot <x0xnofoot@163.com>
2024-02-25 15:32:23 +0800

    add ruby config in env.zsh

--- env.zsh
+++ env.zsh
@@ -32,3 +32,8 @@ export EDITOR='nvim'
 # for xcodebuild
 export TOOLCHAIN_PATH=/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain
 
+# for ruby new version
+if [ -d "/usr/local/opt/ruby/bin" ]; then
+  export PATH=/usr/local/opt/ruby/bin:$PATH
+  export PATH=`gem environment gemdir`/bin:$PATH
+fi
