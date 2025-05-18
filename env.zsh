ddaa2fb917c104df244b03fa6cfdb364922c5231
nofoot <0xnofoot@163.com>
2025-05-18 12:14:59 +0800

    update env

--- env.zsh
+++ env.zsh
@@ -29,15 +29,29 @@ fi
 
 export EDITOR='nvim'
 
-# for trash-cli
-export PATH=$PATH:/opt/homebrew/opt/trash-cli/bin
-
 # for homebrew
 export PATH=$PATH:/opt/homebrew/bin
+
+# for trash-cli
+export PATH=$PATH:/opt/homebrew/opt/trash-cli/bin
 
 # for mason
 export PATH=$PATH:$HOME/.local/share/nvim/mason/bin
 
-# for flutter
+# for flutter with fvm
 export PATH=$PATH:$HOME/fvm/default/bin
 export PATH=$PATH:$HOME/.pub-cache/bin
+
+# for Java
+export JAVA_HOME="$HOME/.local/share/jdk-21.0.2.jdk/Contents/Home"
+export PATH=$PATH:$JAVA_HOME/bin
+
+# for Android
+export ANDROID_HOME=$HOME/.local/share/Android/sdk
+export ANDROID_SDK_ROOT=$ANDROID_HOME
+export PATH=$PATH:$ANDROID_HOME/emulator
+export PATH=$PATH:$ANDROID_HOME/platform-tools
+export PATH=$PATH:$ANDROID_HOME/cmdline-tools/latest/bin
+
+# for Golang with gvm
+[[ -s "/Users/nofoot/.gvm/scripts/gvm" ]] && source "/Users/nofoot/.gvm/scripts/gvm"
